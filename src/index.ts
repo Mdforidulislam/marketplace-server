693d12fde0f2e957276ace1753be2b67a58eb00b
Mdforidulislam <mdforidulislam962@gmail.com>
2025-01-05 23:41:32 +0600

    addednew code here

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,20 @@ import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
 
-
-  
-
-async function main(){
+async function main() {
     try {
-     const database =   await mongoose.connect(config.database_url as string);
+        console.log("Connecting to database at:", config.database_url);
+        const database = await mongoose.connect(config.database_url as string, {
+        });
         console.log("Database connected successfully");
-        // Use server.listen instead of app.listen
+
         app.listen(config.port, () => {
             console.log(`Server is listening on port ${config.port}`);
         });
     } catch (error) {
-        console.log(error);
+        console.error("Failed to connect to the database", error);
     }
 }
 
-main()
-
-
-
+main();
 
